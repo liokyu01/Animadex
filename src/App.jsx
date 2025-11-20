c48858cfc7754eca4e0cbf97ef62691948112122
liokyu01 <leandre.fura@hotmail.fr>
2025-11-20 16:05:45 +0900

    Change page title

--- src/App.jsx
+++ src/App.jsx
@@ -111,7 +111,7 @@ export default function App() {
   return (
     <div className="p-6 bg-gray-50 min-h-screen">
       <div className="max-w-6xl mx-auto">
-        <h1 className="text-3xl font-bold mb-6">Visual Pokédex</h1>
+        <h1 className="text-3xl font-bold mb-6">Léandre's Animadex (Asia)</h1>
 
         <Header
           query={query}
